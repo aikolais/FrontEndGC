2c0eb2cf0c29290c143d4ee74b18a75b6f0eeb18
aikolais <laisdalpuzur@gmail.com>
2022-01-31 18:35:34 -0300

    inicio login

--- src/components/User/Login/LoginComp.tsx
+++ src/components/User/Login/LoginComp.tsx
@@ -4,6 +4,8 @@ import { initializeIcons } from "@fluentui/react";
 import { useAuth } from "../../../context/AuthProvider/useAuth"
 import {useNavigate } from 'react-router-dom';
 
+import './LoginComp.css';
+
 initializeIcons();
 
 const LoginComp = (props: any) => {
@@ -31,18 +33,19 @@ const LoginComp = (props: any) => {
 
     return (
         <div className="user-login">
-            <h1 className="user-login__title">Acessar o Sistema</h1>
-            <form autoComplete="nope"  onSubmit={handleSubmit}>
-                <div className="field">
-                    <label htmlFor="email">E-mail</label>
-                    <input id="email" type="email" name="email" value={email} onChange={e => setEmail(e.target.value)} autoComplete="off"/>
+            <h1 className="user-login__title">Use seus dados para acessar:</h1>
+            <form className="login" autoComplete="nope"  onSubmit={handleSubmit}>
+                <div className="login--form__user">
+                    <label htmlFor="email" id="email-label">E-mail</label>
+                    <input id="email" type="email" name="email" value={email} placeholder="Insira seu e-mail cadastrado" onChange={e => setEmail(e.target.value)} autoComplete="off"/>
+                    <label htmlFor="password" id="password-label">Senha</label>
+                    <input id="password" type="password" name="password" value={password} placeholder="Insira sua senha cadastrada" onChange={e => setPassword(e.target.value)} />
+                <button className="btn-login__user" type="submit">Entrar</button>
                 </div>
-                <div className="field">
-                    <label htmlFor="password">Senha</label>
-                    <input id="password" type="password" name="password" value={password} onChange={e => setPassword(e.target.value)} />
-                </div>
-                <button type="submit">Entrar</button>
             </form>
+            <p className="lost__password">
+                <a href="/">Esqueceu sua senha?</a>
+            </p>
         </div>
     );
 }
