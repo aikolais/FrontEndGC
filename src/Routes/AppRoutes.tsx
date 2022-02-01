aad1554c892ad233d7f0f43ae20d9018b4734363
cassio de vargas oliveira <cassio.vargas@w3k.com.br>
2022-02-01 10:02:36 -0300

    adição da error page

--- src/Routes/AppRoutes.tsx
+++ src/Routes/AppRoutes.tsx
@@ -1,5 +1,5 @@
 import React ,{ useState } from "react";
-import { BrowserRouter, Route, Routes} from "react-router-dom";
+import { BrowserRouter, Route, Routes, Navigate} from "react-router-dom";
 import {AuthProvider} from '../context/AuthProvider';
 
 import Home from "../components/User/Home/HomeComp";
@@ -7,27 +7,51 @@ import Teste from "../components/User/Home/TesteComp";
 import Login from "../components/User/Login/LoginComp";
 import ProtectedLayout from '../components/ProtectedLayout/ILayoutProtected';
 import PublicLayout from '../components/PublicLayout/ILayoutPublic';
+import ErrorPage from '../components/ErroPage/ErrorPage';
 
-import {AuthContext}  from '../context/AuthProvider/index'
+import {AuthContext}  from '../context/AuthProvider/index';
+import { useAuth } from "../context/AuthProvider/useAuth";
+import {  IUser } from '../context/AuthProvider/types';
 
 const AppRoutes = () => {
-
+    
+    const auth = useAuth();
 
     return(
-        <AuthProvider>
             <BrowserRouter>
                     <Routes>
                         <Route element = { <ProtectedLayout/> }>
                             <Route path="/" element = { <Home/>  } />
                             <Route path="/teste" element = { <Teste/>  } />
-                        </Route>
+                        </Route> 
                         <Route element = { <PublicLayout/> }>
                             <Route path="/login" element = { <Login/> } />
                         </Route>
+                        <Route path="*" element = { <ErrorPage/> } />
                     </Routes>
             </BrowserRouter>
-        </AuthProvider>
     )
  };
+
+ const PrivateRoute: React.SFC<{}> = props => {
+    return (
+        <AuthContext.Consumer>
+            {({token}) => (
+                <Route path="/" element={
+                    token ? (
+                        <Route element = { <ProtectedLayout/> }>
+                            <Route path="/" element = { <Home/>  } />
+                            <Route path="/teste" element = { <Teste/>  } />
+                        </Route>
+                    ) : (
+                        <Route element = { <PublicLayout/> }>
+                            <Route path="/login" element = { <Login/> } />
+                        </Route>
+                    )
+                } />
+            )}
+        </AuthContext.Consumer>
+    );
+};
  
  export default AppRoutes;
---
+++ src/components/ErroPage/ErrorPage.tsx
@@ -0,0 +1,12 @@
+import React from "react";
+
+const ErrorPage = (props: any) => {
+
+    return (
+        <div>
+            <h1>Not Found</h1>
+        </div>
+    );
+}
+
+export default ErrorPage;
