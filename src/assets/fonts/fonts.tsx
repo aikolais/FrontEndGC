9374c4604b7c83408462e83336f5df6c9f3d1117
aikolais <laisdalpuzur@gmail.com>
2022-02-01 14:45:16 -0300

    primeiro commit

--- src/assets/fonts/fonts.tsx
+++
@@ -1,7 +0,0 @@
-import { createTheme } from '@fluentui/react';
-
-const AppTheme = createTheme({
-  defaultFontStyle: { fontFamily: 'Microsoft YaHei, PingFang SC, Menlo, Consolas', fontWeight: 'regular' },
-});
-
-export default AppTheme;
--- src/components/User/Login/LoginComp.tsx
+++ src/components/User/Login/LoginComp.tsx
@@ -3,7 +3,7 @@ import { initializeIcons } from "@fluentui/react";
 import { useAuth } from "../../../context/AuthProvider/useAuth"
 import {useNavigate } from 'react-router-dom';
 
-import './LoginComp.css';
+import styles from './LoginComp.module.css';
 
 initializeIcons();
 
@@ -30,30 +30,34 @@ const LoginComp = (props: any) => {
         }
     }
 
-    return (
-        <div className="user-login">
 
-            <h1 className="user-login__title">Use seus dados para acessar:</h1>
-            <form className="login" autoComplete="nope"  onSubmit={handleSubmit}>
-                <div className="login--form__user">
-                    <label htmlFor="email" id="email-label">E-mail</label>
-                    <input id="email" type="email" name="email" value={email} placeholder="Insira seu e-mail cadastrado" onChange={e => setEmail(e.target.value)} autoComplete="off"/>
-                    <label htmlFor="password" id="password-label">Senha</label>
-                    <input id="password" type="password" name="password" value={password} placeholder="Insira sua senha cadastrada" onChange={e => setPassword(e.target.value)} />
-                <button className="btn-login__user" type="submit">Entrar</button>
+
+    return (
+        <div className={styles.userBody}>
+        <div className={styles.userLogin}>
+                <img src={require('../../../assets/images/Logo_Green.png')} alt="Logo GreenDocs" />
+            <h1 className={styles.userLogin}>Use seus dados para acessar:</h1>
+            <form className={styles.login} autoComplete="nope"  onSubmit={handleSubmit}>
+                <div className={styles.loginForm__user}>
+                    <label htmlFor="email" id={styles.email__label}>E-mail</label>
+                    <input id={styles.email} type="email" name="email" value={email} placeholder="Insira seu e-mail cadastrado" onChange={e => setEmail(e.target.value)} autoComplete="off"/>
+                    <label htmlFor="password" id={styles.password__label}>Senha</label>
+                    <input id={styles.password} type="password" name="password" value={password} placeholder="Insira sua senha cadastrada" onChange={e => setPassword(e.target.value)} />
+                <button className={styles.btnLogin__user} type="submit">Entrar</button>
                 </div>
             </form>
 
-            <div className="remember">
-            <input id="remember" type="checkbox" name="remember" value={email} placeholder="Insira seu e-mail cadastrado" onChange={e => setEmail(e.target.value)} autoComplete="off"/>
-            <label htmlFor="remember" id="remember">Lembrar de mim</label>
+            <div className={styles.remember}>
+            <input id={styles.remember} type="checkbox" name="remember" value={email} placeholder="Insira seu e-mail cadastrado" onChange={e => setEmail(e.target.value)} autoComplete="off"/>
+            <label htmlFor="remember" id={styles.remember}>Lembrar de mim</label>
             </div>
 
-            <p className="lost__password">
+            <p className={styles.lost__password}>
                 <a href="/">Esqueceu sua senha?</a>
             </p>
 
             
+        </div>
         </div>
     );
 }
